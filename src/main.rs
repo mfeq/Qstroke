1b958ff524e2ff3c37ced23df14e772aa4066d72
Matthew Blanchard <matthewblanc@msn.com>
2021-02-22 13:47:22 -0500

    Split MFEKMath out into it's own library.

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,190 @@
 #![feature(clamp)]
-
 #![macro_use]
-mod MFEKmath;
-mod variable_width_stroking;
-mod pattern_along_path;
-mod consts;
 
+extern crate MFEKMath;
 use clap::{Arg, App};
-use pattern_along_path::*;
-use MFEKmath::Vector;
-use variable_width_stroking::*;
-
+use MFEKMath::pattern_along_path::*;
+use MFEKMath::vector::Vector;
+use MFEKMath::variable_width_stroking::*;
 use std::fs;
 
 fn main() {
-    /*
-    let matches = App::new("QPaP")
-        .version("0.0.0")
+    let matches = App::new("MFEKStroke")
+        .version("0.0.1")
         .author("Matthew Blanchard <matthewrblanchard@gmail.com")
-        .about("A utility for applying pattern-along-path to ufo files.")
-        .arg(Arg::with_name("path")
-            .long("path")
-            .takes_value(true)
-            .help("The path to the input path file.")
-            .required(true))
-        .arg(Arg::with_name("pattern")
-            .long("pattern")
-            .takes_value(true)
-            .help("The path to the input pattern file.")
-            .required(true))
-        .arg(Arg::with_name("output")
-            .long("out")
-            .takes_value(true)
-            .help("The path where the output will be saved.")
-            .required(true))
-        .arg(Arg::with_name("mode")
-            .short("m")
-            .long("mode")
-            .takes_value(true)
-            .help("<[single|repeated] (single)> set our repeat mode."))
-        .arg(Arg::with_name("scale_x")
-            .long("sx")
-            .takes_value(true)
-            .help("<f64 (1)> how much we scale our input pattern on the x-axis."))
-        .arg(Arg::with_name("scale_y")
-            .long("sy")
-            .takes_value(true)
-            .help("<f64 (1)> how much we scale our input pattern on the y-axis."))
-        .arg(Arg::with_name("subdivide")
-            .short("sub")
-            .long("subdivide")
-            .takes_value(true)
-            .help("<f64 (0)> how many times to subdivide the patterns at their midpoint."))
-        .arg(Arg::with_name("spacing")
-            .long("spacing")
-            .takes_value(true)
-            .help("<f64 (0)> how much padding to trail each copy with."))
-        .arg(Arg::with_name("normal_offset")
-            .long("noffset")
-            .takes_value(true)
-            .help("<f64 (0)> how much to offset the pattern along the normal of the path."))
-        .arg(Arg::with_name("tangent_offset")
-            .long("toffset")
-            .takes_value(true)
-            .help("<f64 (0)> how much to offset the pattern along the tangent of the path."))
-        .arg(Arg::with_name("stretch")
-            .long("stretch")
-            .takes_value(true)
-            .help("<boolean (false)> whether to stretch the input pattern or not."))
-        .arg(Arg::with_name("simplify")
-            .long("simplify")
-            .takes_value(true)
-            .help("<boolean (false)> if we should run the result through a simplify routine."))
-        .arg(Arg::with_name("center_pattern")
-            .long("center_pattern")
-            .takes_value(true)
-            .help("<boolean (true)> if you want to align a pattern manually you can change this to false."))
+        .about("A utility for applying stroking techniques to UFO contours.")
+        .subcommand(App::new("PAP")
+            .about("Maps a pattern glyph along a path glyph.")
+            .version("0.1")
+            .author("Matthew Blanchard <matthewrblanchard@gmail.com")
+            .arg(Arg::with_name("path")
+                .long("path")
+                .takes_value(true)
+                .help("The path to the input path file.")
+                .required(true))
+            .arg(Arg::with_name("pattern")
+                .long("pattern")
+                .takes_value(true)
+                .help("The path to the input pattern file.")
+                .required(true))
+            .arg(Arg::with_name("output")
+                .long("out")
+                .takes_value(true)
+                .help("The path where the output will be saved.")
+                .required(true))
+            .arg(Arg::with_name("mode")
+                .short("m")
+                .long("mode")
+                .takes_value(true)
+                .help("<[single|repeated] (single)> set our repeat mode."))
+            .arg(Arg::with_name("scale_x")
+                .long("sx")
+                .takes_value(true)
+                .help("<f64 (1)> how much we scale our input pattern on the x-axis."))
+            .arg(Arg::with_name("scale_y")
+                .long("sy")
+                .takes_value(true)
+                .help("<f64 (1)> how much we scale our input pattern on the y-axis."))
+            .arg(Arg::with_name("subdivide")
+                .short("sub")
+                .long("subdivide")
+                .takes_value(true)
+                .help("<f64 (0)> how many times to subdivide the patterns at their midpoint."))
+            .arg(Arg::with_name("spacing")
+                .long("spacing")
+                .takes_value(true)
+                .help("<f64 (0)> how much padding to trail each copy with."))
+            .arg(Arg::with_name("normal_offset")
+                .long("noffset")
+                .takes_value(true)
+                .help("<f64 (0)> how much to offset the pattern along the normal of the path."))
+            .arg(Arg::with_name("tangent_offset")
+                .long("toffset")
+                .takes_value(true)
+                .help("<f64 (0)> how much to offset the pattern along the tangent of the path."))
+            .arg(Arg::with_name("stretch")
+                .long("stretch")
+                .takes_value(true)
+                .help("<boolean (false)> whether to stretch the input pattern or not."))
+            .arg(Arg::with_name("simplify")
+                .long("simplify")
+                .takes_value(true)
+                .help("<boolean (false)> if we should run the result through a simplify routine."))
+            .arg(Arg::with_name("center_pattern")
+                .long("center_pattern")
+                .takes_value(true)
+                .help("<boolean (true)> if you want to align a pattern manually you can change this to false."))
+        )
+        .subcommand(App::new("VWS")
+            .about("Takes a UFO file and strokes it with variable width.")
+            .version("0.0")
+            .author("Matthew Blanchard <matthewrblanchard@gmail.com")
+            .arg(Arg::with_name("input")
+                .short("in")
+                .takes_value(true)
+                .help("The path to the input file.")
+                .required(true))
+            .arg(Arg::with_name("output")
+                .short("out")
+                .takes_value(true)
+                .help("The path where the output will be saved.")
+                .required(true))
+            .arg(Arg::with_name("join_type")
+                .short("jt")
+                .long("join_type")
+                .takes_value(true)
+                .help("<bevel/miter/round> which type of join to use."))
+            .arg(Arg::with_name("cap_type_start")
+                .long("cts")
+                .takes_value(true)
+                .help("<round/square/path to custom cap> which type of cap to use for the starting cap."))
+            .arg(Arg::with_name("cap_type_end")
+                .long("noffset")
+                .takes_value(true)
+                .help("<round/square/path to custom cap> which type of cap to use for the ending cap."))
+        )
         .get_matches();
 
+    if let Some(ref matches) = matches.subcommand_matches("PAP") {
+        pap_cli(matches);
+    }
+ 
+    if let Some(ref matches) = matches.subcommand_matches("VWS") {
+        vws_cli(matches);
+    }
+    
+}
+
+fn vws_cli(matches: &clap::ArgMatches)
+{
+    let input_string = matches.value_of("input").unwrap();
+    let output_string = matches.value_of("output").unwrap();
+
+    let input: glifparser::Glif<Option<MFEKMath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(input_string)
+    .expect("Failed to read path file!"));
+
+    let mut settings = VWSSettings {
+        join_type: JoinType::Bevel,
+        cap_type_start: CapType::Square,
+        cap_type_end: CapType::Square,
+        cap_custom_end: None,
+        cap_custom_start: None
+    };
+
+    if let Some(joins) = matches.value_of("join_type") { 
+        match joins {
+            "bevel" => settings.join_type = JoinType::Bevel,
+            "round" => settings.join_type = JoinType::Round,
+            "miter" => settings.join_type = JoinType::Miter,
+            _ => eprintln!("Invalid join type argument. Falling back to default. (Bevel)")
+        }
+    }
+
+    if let Some(capstart) = matches.value_of("cap_type_start") { 
+        match capstart {
+            "round" => settings.cap_type_start = CapType::Round,
+            "square" => settings.cap_type_start = CapType::Square,
+            _ => {
+                // if our input isn't round or square it should be a path to a glif
+                let cap: glifparser::Glif<Option<MFEKMath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(capstart)
+                    .expect("Failed to read cap start file!"));
+                
+                settings.cap_type_start = CapType::Custom;
+                settings.cap_custom_start = Some(cap);
+            }
+        }
+    }
+
+    if let Some(capend) = matches.value_of("cap_type_end") { 
+        match capend {
+            "round" => settings.cap_type_end = CapType::Round,
+            "square" => settings.cap_type_end = CapType::Square,
+            _ => {
+                // if our input isn't round or square it should be a path to a glif
+                let cap: glifparser::Glif<Option<MFEKMath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(capend)
+                    .expect("Failed to read cap start file!"));
+                
+                settings.cap_type_end = CapType::Custom;
+                settings.cap_custom_end = Some(cap);
+            }
+        }
+    }
+
+    let out = variable_width_stroke_glif(&input, settings);
+    let glifstring = glifparser::write_ufo_glif(out);
+    fs::write(output_string, glifstring).expect("Unable to write file");
+}
+
+fn pap_cli(matches: &clap::ArgMatches)
+{
     let path_string = matches.value_of("path").unwrap(); // required options shouldn't panic?
     let pattern_string = matches.value_of("pattern").unwrap();
     let output_string = matches.value_of("output").unwrap();
 
-    let path: glifparser::Glif<Option<MFEKmath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(path_string)
+    let path: glifparser::Glif<Option<MFEKMath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(path_string)
     .expect("Failed to read path file!"));
  
-    let pattern: glifparser::Glif<Option<MFEKmath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(pattern_string)
+    let pattern: glifparser::Glif<Option<MFEKMath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string(pattern_string)
         .expect("Failed to read pattern file!"));
 
 
@@ -192,13 +291,4 @@ fn main() {
     let output = pattern_along_glif(&path, &pattern, &settings);
     let glifstring = glifparser::write_ufo_glif(output);
     fs::write(output_string, glifstring).expect("Unable to write file");
-    */
-
-    let path: glifparser::Glif<Option<MFEKmath::piecewise::glif::PointData>> = glifparser::read_ufo_glif(&fs::read_to_string("Q_.glif")
-    .expect("Failed to read path file!"));
- 
-    let out = variable_width_stroke_glif(&path);
-    let glifstring = glifparser::write_ufo_glif(out);
-    fs::write("O_stroked.glif", glifstring).expect("Unable to write file");
-    
 }
